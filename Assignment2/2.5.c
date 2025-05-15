26664726736d86011c917cb7f9a3cc7d79db8bab
Pallavi-asus <pallavisable057@gmail.com>
2025-05-15 12:29:05 +0530

    Add files via upload

---
+++ Assignment2/2.5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+BOOL ChkEven(int iNo)
+{
+    if((iNo%2==0))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+int main()
+{
+    int iValue=0;
+    BOOL bRet=FALSE;
+    printf("Enter Number");
+    scanf("%d",&iValue);
+    bRet=ChkEven(iValue);
+    if(bRet==TRUE)
+    {
+        printf("Your Number is Even");
+    }
+    else
+    {
+        printf("Your Number is Odd");
+    }    
+    return 0;
+}
